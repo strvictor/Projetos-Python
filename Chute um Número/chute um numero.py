736d60bb83ee21ea4443995518a67fc18c530e9f
Victor Silva <vicctor1009@gmail.com>
2021-12-16 19:55:11 -0300

    terminado
    primeira versão ta pronta

--- Chute um Número/chute um numero.py
+++ Chute um Número/chute um numero.py
@@ -1,3 +1,20 @@
 from random import randint
-valor_aleatorio = randint(0,50)
-print(valor_aleatorio)
+valor_aleatorio = int(randint(0,50))
+tentativas = 0
+while True:
+    print('=' * 30)
+    resp_usuario = int(input('Chute um número em 0 e 50: '))
+    print('=' * 30)
+    if resp_usuario > valor_aleatorio:
+        print('')
+        print(f'Seu ultimo chute foi {resp_usuario}, chute um valor mais baixo')
+        print('')
+    elif resp_usuario < valor_aleatorio:
+        print('')
+        print(f'Seu ultimo chute foi {resp_usuario}, chute um valor mais alto')
+        print('')
+    elif resp_usuario == valor_aleatorio:
+        print('')
+        print(f'Parabéns voce acertou!\nao todo foram {tentativas} tentativas')
+        print('')
+    tentativas += 1
