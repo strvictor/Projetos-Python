3b48d3ae507355aa48586458b7f9db2db3da0753
Victor Silva <vicctor1009@gmail.com>
2021-12-22 00:35:48 -0300

    tentando concertar o bug
    fail

--- Organizando arquivos automaticamente/organizando arquivos.py
+++ Organizando arquivos automaticamente/organizando arquivos.py
@@ -2,8 +2,12 @@ import os
 from pathlib import Path
 import shutil
 # chegar na pasta area de trabalho
+lista = []
+lista_arquivo = []
+barra = \
 desktop = r'C:\Users\Victor\Desktop'
 os.chdir(desktop)
+
 
 # ler os arquivos da pasta
 caminho = Path(desktop)
@@ -11,12 +15,26 @@ pastapdf = r"C:\Users\Victor\Desktop\pdf's"
 pastacdr = r"C:\Users\Victor\Desktop\corel's"
 pastapng = r"C:\Users\Victor\Desktop\png's"
 arquivos = os.listdir(desktop)
+print(caminho + barra)
 
+'''for arquivo in arquivos:
+    lista_arquivo.append(arquivo)
+    
+    for nomearquivo in caminho.glob('*'):
+        extensao = nomearquivo.suffix
+        lista.append(extensao)
+        if lista[-1] == '.pdf':
+            source = path_completo + arquivo
+            print(path_completo)
+            #mover = shutil.move(source, pastapdf)
+            print('Arquivo pdf movido')
+        else:
+            print('nao foi')'''
 
 # verificar a extensão dos arquivos
-for nomearquivo in caminho.glob('*'):
-    extensao = nomearquivo.suffix
-    for arquivo in arquivos:
+
+# mover os arquivos de cada extensão pra pasta definida
+'''for arquivo in arquivos:
         if extensao == '.pdf':
             shutil.move(caminho+arquivo, pastapdf)
             print('Arquivo pdf movido')
@@ -25,5 +43,4 @@ for nomearquivo in caminho.glob('*'):
             print('arquivo .cdr')
         elif extensao == '.png' or '.PNG' or '.jpg' or '.JPG':
             shutil.move(caminho+arquivo, pastapng)
-            print('arquivo .jpg ou .png')
-# mover os arquivos de cada extensão pra pasta definida
+            print('arquivo .jpg ou .png')'''
