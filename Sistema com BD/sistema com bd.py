0b81d82f52719f90a896d40716300106672748aa
Victor Silva <vicctor1009@gmail.com>
2022-06-22 00:26:59 -0300

    implementações

--- Sistema com BD/sistema com bd.py
+++ Sistema com BD/sistema com bd.py
@@ -3,45 +3,61 @@ import json
 database_cadastro = {}
 database_login = {}
 while True:
+    print('')
     print('[ 1 ] - Cadastrar no sistema\n[ 2 ] - Entrar no sistema\n[ 3 ] - Sair do sistema')
+    print('')
     opc = int(input('Sua opção: '))
     if opc == 1:
+        print("==============================")
         database_cadastro['Usuario'] = input('Usuário: ')
         database_cadastro['Senha'] = input('Senha: ')
         conversao = json.dumps(database_cadastro)
-        requisicao = requests.post('https://teste2-d5c1f-default-rtdb.firebaseio.com/.json', data=conversao)
+        requisicao = requests.post('https://bancodedados-54d83-default-rtdb.firebaseio.com/.json', data=conversao)
+        print("================================")
         print('Cadastro realizado com sucesso!')
+        print("================================")
+ 
     elif opc == 2:
+        print("")
         print('Informe seus dados abaixo')
+        print("")
         database_login['Usuario'] = input('Usuário: ')
-        requisicao_login = requests.get('https://teste2-d5c1f-default-rtdb.firebaseio.com/.json')
-        caminhos = requisicao_login.json()
-        for caminho in caminhos:
-            requisicao_login = requests.get(f'https://teste2-d5c1f-default-rtdb.firebaseio.com/{caminho}.json')
-            verifica = requisicao_login.json()
+        
+        requisicao_login = requests.get('https://bancodedados-54d83-default-rtdb.firebaseio.com/.json') #get no banco de dados
+        caminhos = requisicao_login.json() # banco de dados no formato .json
+        for caminho in caminhos: # pra percorrer em todo o banco de dados
+            
+            requisicao_login = requests.get(f'https://bancodedados-54d83-default-rtdb.firebaseio.com/{caminho}.json') # add o parametro pra percorrer o banco de dados
+            
+            verifica = requisicao_login.json() #mostra todos os usuarios do banco de dados
+            
             if database_login['Usuario'] in verifica['Usuario']:
-                while True:
-                    database_login['Senha'] = input('Senha: ')
-                    if database_login['Senha'] == verifica['Senha']:
-                        print('Login realizado com sucesso!')
-                        break
+                
+               
+                database_login['Senha'] = input('Senha: ')
+                
+                if database_login['Senha'] == verifica['Senha']:
+                    print('')
+                    print('*'*28)
+                    print('Login realizado com sucesso!')
+                    print('*'*28)
+                    break
                     
-                    else:
-                        print('Senha incorreta, quer tentar novamente ?\n[ 1 ] - para tentar novamente [ 2 ] - para sair')
-                        opc2 = int(input('Sua opção: '))
-                        if opc2 == 2:
-                            break
+                else:
+                    print("=======================")
+                    print('Senha Incorreta')
+                    print("=======================")
+                    break
+              
+        if database_login['Usuario'] not in verifica['Usuario']:
+            print("=======================")
+            print('Usuário não encontrado!')
+            print("=======================")
+                
     elif opc == 3:
-        print('Sistema encerrado.\nObrigado(a)!')
+        print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
+        print(f'Sistema encerrado.\nObrigado(a) {database_login["Usuario"]}!')
+        print('=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-')
         break
     else:
         print('Só é permitido um dos valores abaixo!')
-        
-        
-def Verifica_Usuario():
-    database_login['Senha'] = input('Senha: ')
-    if database_login['Senha'] == verifica['Senha']:
-        print('Login realizado com sucesso!')
-    else:
-        print('Senha incorreta, quer tentar novamente ?\n[ 1 ] - para tentar novamente [ 2 ] - para sair')
-        opc2 = int(input('Sua opção: '))
